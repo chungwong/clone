0584f3ee405d34c8bdc822482d0e9a9d59d550a5
Chung Wong <chung@sydneytools.com.au>
2022-09-16 10:22:46 +1000

    convert npc to traitless

--- src/npc.rs
+++ src/npc.rs
@@ -8,14 +8,69 @@ use crate::{
     state::{ConditionSet, GameState},
     tilemap::{
         ldtk_pixel_coords_to_translation_pivoted, EntityInstance, FieldValue, LayerInstance,
-        LdtkEntity, RegisterLdtkObjects, TilesetDefinition,
+        LdtkLevel,
     },
 };
 
 #[derive(Clone, Component, Default)]
 pub(crate) struct Enemy;
 
-#[derive(Bundle, Clone, Default, LdtkEntity)]
+impl Enemy {
+    fn despawn(
+        mut cmd: Commands,
+        enemies: Query<(Entity, &Health), (With<Enemy>, Changed<Health>)>,
+    ) {
+        for (entity, health) in enemies.iter() {
+            if health.current == 0 {
+                cmd.entity(entity).despawn_recursive();
+            }
+        }
+    }
+
+    fn spawn_npc(
+        mut cmd: Commands,
+        entity_query: Query<(Entity, &Transform, &EntityInstance), Added<EntityInstance>>,
+        level_query: Query<&Handle<LdtkLevel>>,
+        levels: Res<Assets<LdtkLevel>>,
+        asset_server: Res<AssetServer>,
+    ) {
+        for (entity, transform, entity_instance) in entity_query.iter() {
+            if entity_instance.identifier == *"Mob" {
+                let layer_instance: &LayerInstance = level_query
+                    .iter()
+                    .find_map(|handle| {
+                        levels
+                            .get(handle)
+                            .expect("Level should be loaded by this point")
+                            .level
+                            .layer_instances
+                            .iter()
+                            .flatten()
+                            .find(|layer| layer.identifier == "Mobs")
+                    })
+                    .expect("Mobs layer is not found in Ldtk file");
+
+                cmd.entity(entity).insert_bundle(MobBundle {
+                    sprite_bundle: SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::RED,
+                            ..default()
+                        },
+                        texture: asset_server.load("player.png"),
+                        transform: *transform,
+                        ..default()
+                    },
+                    collider_bundle: entity_instance.into(),
+                    hp: entity_instance.into(),
+                    patrol: Patrol::new(entity_instance, layer_instance),
+                    ..default()
+                });
+            }
+        }
+    }
+}
+
+#[derive(Bundle, Clone, Default)]
 pub(crate) struct MobPhysicsBundle {
     pub(crate) collider: Collider,
     pub(crate) collider_mass_properties: ColliderMassProperties,
@@ -30,8 +85,8 @@ pub(crate) struct MobPhysicsBundle {
     pub(crate) ccd: Ccd,
 }
 
-impl From<EntityInstance> for MobPhysicsBundle {
-    fn from(entity_instance: EntityInstance) -> Self {
+impl From<&EntityInstance> for MobPhysicsBundle {
+    fn from(entity_instance: &EntityInstance) -> Self {
         Self {
             collider: Collider::cuboid(
                 entity_instance.width as f32 / 2.0,
@@ -54,22 +109,17 @@ impl From<EntityInstance> for MobPhysicsBundle {
     }
 }
 
-#[derive(Clone, Default, Bundle, LdtkEntity)]
+#[derive(Clone, Default, Bundle)]
 pub(crate) struct MobBundle {
-    #[sprite_bundle("player.png")]
     #[bundle]
     pub(crate) sprite_bundle: SpriteBundle,
 
-    #[from_entity_instance]
     #[bundle]
     pub(crate) collider_bundle: MobPhysicsBundle,
 
     pub(crate) enemy: Enemy,
 
-    #[from_entity_instance]
     pub(crate) hp: Health,
-
-    #[ldtk_entity]
     pub(crate) patrol: Patrol,
 }
 
@@ -80,15 +130,8 @@ pub(crate) struct Patrol {
     pub(crate) forward: bool,
 }
 
-impl LdtkEntity for Patrol {
-    fn bundle_entity(
-        entity_instance: &EntityInstance,
-        layer_instance: &LayerInstance,
-        _: Option<&Handle<Image>>,
-        _: Option<&TilesetDefinition>,
-        _: &AssetServer,
-        _: &mut Assets<TextureAtlas>,
-    ) -> Self {
+impl Patrol {
+    fn new(entity_instance: &EntityInstance, layer_instance: &LayerInstance) -> Self {
         let mut points = vec![ldtk_pixel_coords_to_translation_pivoted(
             entity_instance.px,
             layer_instance.c_hei * layer_instance.grid_size,
@@ -121,10 +164,44 @@ impl LdtkEntity for Patrol {
             }
         };
 
-        Patrol {
+        Self {
             points,
             index: 1,
             forward: true,
+        }
+    }
+
+    fn patrol(mut query: Query<(&mut Transform, &mut Velocity, &mut Patrol)>) {
+        for (mut transform, mut velocity, mut patrol) in query.iter_mut() {
+            if patrol.points.len() <= 1 {
+                continue;
+            }
+
+            let mut new_velocity =
+                (patrol.points[patrol.index] - transform.translation.truncate()).normalize() * 75.0;
+
+            if new_velocity.dot(velocity.linvel) < 0. {
+                if patrol.index == 0 {
+                    patrol.forward = true;
+                } else if patrol.index == patrol.points.len() - 1 {
+                    patrol.forward = false;
+                }
+
+                transform.translation.x = patrol.points[patrol.index].x;
+                transform.translation.y = patrol.points[patrol.index].y;
+
+                if patrol.forward {
+                    patrol.index += 1;
+                } else {
+                    patrol.index -= 1;
+                }
+
+                new_velocity = (patrol.points[patrol.index] - transform.translation.truncate())
+                    .normalize()
+                    * 75.0;
+            }
+
+            velocity.linvel = new_velocity;
         }
     }
 }
@@ -133,66 +210,13 @@ pub struct NpcPlugin;
 
 impl Plugin for NpcPlugin {
     fn build(&self, app: &mut App) {
-        app.register_ldtk_entity::<MobBundle>("Mob")
-            .add_system_set(
-                ConditionSet::new()
-                    .run_in_state(GameState::LoadingLevel)
-                    .with_system(setup)
-                    .into(),
-            )
-            .add_system_set(
-                ConditionSet::new()
-                    .run_in_state(GameState::InGame)
-                    .with_system(despawn)
-                    .with_system(patrol)
-                    .into(),
-            );
-    }
-}
-
-fn setup(mut enemies: Query<&mut Sprite, Added<Enemy>>) {
-    for mut sprite in enemies.iter_mut() {
-        sprite.color = Color::RED;
-    }
-}
-
-fn despawn(mut cmd: Commands, enemies: Query<(Entity, &Health), (With<Enemy>, Changed<Health>)>) {
-    for (entity, health) in enemies.iter() {
-        if health.current == 0 {
-            cmd.entity(entity).despawn_recursive();
-        }
-    }
-}
-
-fn patrol(mut query: Query<(&mut Transform, &mut Velocity, &mut Patrol)>) {
-    for (mut transform, mut velocity, mut patrol) in query.iter_mut() {
-        if patrol.points.len() <= 1 {
-            continue;
-        }
-
-        let mut new_velocity =
-            (patrol.points[patrol.index] - transform.translation.truncate()).normalize() * 75.0;
-
-        if new_velocity.dot(velocity.linvel) < 0. {
-            if patrol.index == 0 {
-                patrol.forward = true;
-            } else if patrol.index == patrol.points.len() - 1 {
-                patrol.forward = false;
-            }
-
-            transform.translation.x = patrol.points[patrol.index].x;
-            transform.translation.y = patrol.points[patrol.index].y;
-
-            if patrol.forward {
-                patrol.index += 1;
-            } else {
-                patrol.index -= 1;
-            }
-
-            new_velocity =
-                (patrol.points[patrol.index] - transform.translation.truncate()).normalize() * 75.0;
-        }
-
-        velocity.linvel = new_velocity;
+        app.add_system_set(
+            ConditionSet::new()
+                .run_in_state(GameState::InGame)
+                .with_system(Enemy::spawn_npc)
+                .with_system(Enemy::despawn)
+                .with_system(Patrol::patrol)
+                .into(),
+        );
     }
 }
